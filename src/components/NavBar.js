b489ba19084917351fab130e13a0f214892d879a
clinton <clinton.wambua@student.moringaschool.com>
2023-04-20 20:44:42 +0300

    complete

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,12 +3,15 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return;
+  return(
   <nav>
-    <a href="# home">home</a>
-    <a href="# about">about</a>
-    <a href="# projects">projects</a>
-  </nav>;
+    <div>
+    <a href="#home">home</a>
+    <a href="#about">about</a>
+    <a href="#projects">projects</a>
+    </div>
+  </nav>
+  );
 }
 
 export default NavBar;
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -9,7 +9,7 @@ function ProjectList({ projects }) {
       <div id="project-list">{/* render ProjectItem components here */}
       {projects.map((project) => {
           return  <ProjectItem
-          key={project.id}q
+          key={project.id}
           name={project.name}
           about={project.about}
           technologies={project.technologies}
